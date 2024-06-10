9e0a66bf9b3821cbf355d9a79aa5f302b5d448db
rkanishka <kanishkaraghuvanshi97@gmail.com>
2024-06-09 21:58:39 -0700

    continous delivery

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('3904bb5b35d24000b3e68c93b8aa8c8b').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -21,3 +24,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//atatus.notify(new Error('Test Atatus Setup'));
